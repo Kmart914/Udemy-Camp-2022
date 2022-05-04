615db96a6795c1816488d05ae2fc689ada33ea62
Kevin Martzolf <Kmart914@gmail.com>
2022-05-04 16:19:17 -0700

    Built Todo list

--- JavaScript/ToDo List/app.js
+++ JavaScript/ToDo List/app.js
@@ -1,9 +1,28 @@
-let action = prompt("What would you like to do?");
+let input = prompt("What would you like to do?");
 
-let list = [];
+const todos = ["Test"];
 
-console.log(action.toLowerCase());
 
-if (action.toLowerCase() === "new"){
-  prompt("What would you like to add to the list?")
+while(input.toLowerCase() !== 'quit' && input !== 'q') {
+  if(input.toLowerCase() === "list"){
+    for(let i = 0; i < todos.length; i++){
+      console.log(`${i}: ${todos[i]}`);
+    }
+  } else if (input.toLowerCase() === "new"){
+    const newTodo = prompt("Okay, what is the new todo?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to the list`);
+  } else if (input === "delete") {
+    const index = parseInt(prompt("Ok, enter an index to delete"));
+    if(!Number.isNaN(index)){
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok, deleted ${deleted[0]}`);
+    } else {
+      console.log("Unknown Index");
+    }
+
+  }
+  input = prompt("What would you like to do?");
 }
+
+console.log("Ok, you quit the app");
